cc76f7b1b695f929fee6371300e739da59b2312c
Giedrius Balčas <giedrius.balcas@gmail.com>
2025-05-13 20:12:11 +0300

    feat: added API gateway, added ms sql server to kubernetes, updated command service

--- CommandService/Controllers/PlatformsController.cs
+++ CommandService/Controllers/PlatformsController.cs
@@ -1,15 +1,33 @@
 using System;
+using System.Collections.Generic;
+using AutoMapper;
+using CommandService.Data;
+using CommandService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CommandsService.Controllers
+namespace CommandService.Controllers
 {
     [Route("api/c/[controller]")]
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
+        private readonly ICommandRepo _commandRepo;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(ICommandRepo commandRepo, IMapper mapper)
         {
-            
+            _commandRepo = commandRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms from CommandService");
+
+            var platforms = _commandRepo.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
         }
 
         [HttpPost]
